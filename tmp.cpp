4030ed38e12111e3703c2aee066ac63b593e49ce
YoheiSaito <teyo.13324@gmail.com>
2018-12-08 19:00:06 +0900

    add tmp.c as test main source

--- tmp.cpp
+++ tmp.cpp
@@ -1,56 +1,23 @@
 #include <iostream>
-#include <boost/asio.hpp>
 #include <unistd.h>
-
-namespace asio = boost::asio;
-using asio::ip::tcp;
-
-void receive(tcp::socket &socket){
-	asio::streambuf receive_buffer;
-	boost::system::error_code error;
-
-	asio::read(socket, receive_buffer, asio::transfer_at_least(1), error);
-
-	const char* data = asio::buffer_cast<const char*>(receive_buffer.data());
-	if (error && error != asio::error::eof) {
-		std::cout << "receive failed: " << error.message() << std::endl;
-	}
-	else {
-		const char* data = asio::buffer_cast<const char*>(receive_buffer.data());
-		std::cout << data << std::endl;
-	}
-}
-void send(tcp::socket& socket, std::string const& msg){
-	boost::system::error_code error;
-	asio::write(socket, asio::buffer(msg+"\n"), error);
-	if (error) {
-		std::cout << "send failed: " << error.message() << std::endl;
-	}
-	else {
-		std::cout << "send correct!" << std::endl;
-	}
-}
-int main()
+#include "command.hpp"
+int main(int argc, char* argv[])
 {
-	asio::io_service io_service;
-	tcp::socket socket(io_service);
-
-	boost::system::error_code error;
-	socket.connect(tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 4444), error);
-
-	if (error) {
-		std::cout << "connect failed : " << error.message() << std::endl;
-	}
-	else {
-		std::cout << "connected" << std::endl;
-	}
-	receive(socket);
-	while(1){
-		std::string msg;
-		std::cin >> msg;
-		send(socket, msg);
-		usleep(1e4);
-		receive(socket);
+	try{
+		boost::asio::io_service io_service;
+		boost::asio::ip::tcp::socket socket(io_service);
+		teyo_shogi::Command cmd(socket);
+		int self = cmd.connect(NULL, 4444);
+		while(1){
+			while( self != cmd.turn()){
+				std::cout << "not my turn" << std::endl;
+				usleep(1e5);
+			}
+			if( !cmd.move("B4","A3"))
+				cmd.move("A3","B4");
+		}
+	} catch(std::string e){
+		std::cerr << e << std::endl;
 	}
 
 }
